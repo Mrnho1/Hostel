104b9ecd992d0fce9138683818b525d78dc70d8c
lucas da silva marinho <ls.marinho36@gmail.com>
2024-05-12 08:50:07 -0300

    chore: create components and a Angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { QuemSomosComponent } from './components/quem-somos/quem-somos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuemSomosComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
